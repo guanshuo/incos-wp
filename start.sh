8dd5fb57eb714bdebbd33954656760d31d7c67a5
冠硕 <12610446@qq.com>
2016-08-25 23:42:00 +0800

    new file

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ ! -d /data/www ]; then
+  mkdir data/www && cd /data/www/ && git init
+  git remote add origin http://guanshuo:31415926@git.oschina.net/guanshuo/WordPress-php7.git
+  git pull origin master
+else
+  cd /data/www/ && git init
+  git pull origin master
+fi
+cp -f /data/www/configs/run.sh /run.sh && sed -i -e 's/\r//g' /run.sh && sed -i -e 's/^M//g' /run.sh && chmod +x /*.sh
+/run.sh
